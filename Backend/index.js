86fdc2e06219b103204975863078a5708d4147a7
panwarnikita <nikitapanwar593@gmail.com>
2025-09-08 22:55:03 +0530

    final delpoyment

--- Backend/index.js
+++ Backend/index.js
@@ -136,7 +136,7 @@ cron.schedule('*/3 * * * *', async () => {
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Server started on https://kitchen-flow.onrender.com:${port}`);
+  console.log(`🚀 Server started on 5000:${port}`);
 });
 
 
