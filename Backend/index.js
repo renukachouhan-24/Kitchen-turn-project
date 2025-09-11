54dc11b869bd94b53eed5af36371ba2fc5011b37
Renuka <renukachouhan24@navgurukul.org>
2025-09-12 00:42:53 +0530

    Added final logic

--- Backend/index.js
+++ Backend/index.js
@@ -230,7 +230,7 @@ app.post('/api/skip-rotation', (req, res) => {
 });
 
 
-cron.schedule('*/10 * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
   // Check if the skip flag is set
   if (skipRotationToday) {
     console.log('🗓️ Rotation skipped via manual command.');
@@ -263,7 +263,7 @@ cron.schedule('*/10 * * * *', async () => {
   }
 });
 
-cron.schedule('*/10 * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     console.log('🧹 Running daily data reset task...');
     try {
         await Menu.deleteMany({});
@@ -277,9 +277,9 @@ cron.schedule('*/10 * * * *', async () => {
     }
 });
 
-cron.schedule('*/10 * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     console.log('⏳ Running cron job to delete old resolved skip requests...');
-    const timeLimitInMs =  10 * 60 * 1000;
+    const timeLimitInMs =  24 * 60 * 60 * 1000;
     const timeThreshold = new Date(Date.now() - timeLimitInMs);
     
     try {
