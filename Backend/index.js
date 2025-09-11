f4fb69f50fa88014ba89ff9494de5929b04ad18b
Renuka <renukachouhan24@navgurukul.org>
2025-09-12 01:33:24 +0530

    Added new render URL

--- Backend/index.js
+++ Backend/index.js
@@ -294,5 +294,5 @@ cron.schedule('0 0 * * *', async () => {
 });
 
 app.listen(port, () => {
-  console.log(`🚀 http://localhost:${port}`);
+  console.log(`🚀 https://kitchen-turn-project-4.onrender.com`);
 });
